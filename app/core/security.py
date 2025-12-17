b77e34721eea272c4fb9936fdcb94f0125c12202
baxtiyorovbekzod <bekzodbaxtiyorov142@gmail.com>
2025-12-17 23:06:23 +0500

    ok

--- app/core/security.py
+++ app/core/security.py
@@ -5,10 +5,11 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-def hash_password(plain_password: str) -> str:
-    """Hashes the plain-text password."""
-    return pwd_context.hash(plain_password)
 
+
+def hash_password(plain_password: str) -> str:
+    ok = plain_password[:72]  
+    return pwd_context.hash(ok)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Verifies a plain-text password against a hashed password."""
